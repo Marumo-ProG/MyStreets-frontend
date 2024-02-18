2f857b68a091302f1e3a343a8215bd17972c4a30
Marumo-ProG <Lennythobejane@gmail.com>
2024-02-18 08:45:46 +0200

    file structure

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+// this file will hold all contant data that we want to maintain accross the platform
+
+export const colors = {
+  dark_brown: "#D87D4A",
+  light_black: "#101010",
+  med_grey: "#F1F1F1",
+  light_grey: "#FAFAFA",
+  peach: "#FBAF85",
+  white: "#FFFFFF",
+  black: "#000000",
+};
