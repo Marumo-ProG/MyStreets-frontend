1b7c2e5fdc8d06926b81182f56a869106005463f
Marumo-ProG <Lennythobejane@gmail.com>
2024-02-21 04:23:35 +0200

    products page done

--- src/pages/headphones/index.js
+++ src/pages/headphones/index.js
@@ -4,9 +4,12 @@ import Stack from "@mui/material/Stack";
 // components
 import PageTitleMiniBanner from "../components/PageTitleMiniBanner";
 import ProductBlock from "../components/ProductBlock";
+import OverviewProductList from "../overview/containers/OverviewProductList";
+import TestimonialBanner from "../overview/containers/TestimonialBanner";
 
 // Images
 import headphoneImage from "../../images/product1.png";
+import TestimonyImage from "../../images/productTestimony.png";
 
 const Headphones = () => {
   return (
@@ -21,6 +24,37 @@ const Headphones = () => {
         }
         isNew={true}
         link="products/23"
+      />
+      <ProductBlock
+        title="Lenny's phones"
+        image={headphoneImage}
+        isInverted={false}
+        description={
+          "Come check this awesome thing out, you will get your mind blown ksnakfnaskdnasd"
+        }
+        isNew={true}
+        link="products/23"
+      />
+      <ProductBlock
+        title="Lenny's phones"
+        image={headphoneImage}
+        isInverted={true}
+        description={
+          "Come check this awesome thing out, you will get your mind blown ksnakfnaskdnasd"
+        }
+        isNew={true}
+        link="products/23"
+      />
+      <Stack marginTop={"180px"}>
+        <OverviewProductList />
+      </Stack>
+
+      <TestimonialBanner
+        title="Bringing you the best audio gear"
+        image={TestimonyImage}
+        description={
+          "Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment."
+        }
       />
     </Stack>
   );
